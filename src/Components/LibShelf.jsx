a581320db2ab9ebeafad2f1f4ea5ffb2b3121c1f
Vivek-Rathod14 <rathodvivek5500@gmail.com>
2026-03-24 11:39:40 +0530

    fixed: delete / edit  async

--- src/Components/LibShelf.jsx
+++ src/Components/LibShelf.jsx
@@ -17,7 +17,7 @@ export const LibShelf = () => {
     return (
         <>
             <div className='flex flex-row flex-wrap'>
-                {books.map(((book, i) => {
+                {books.map(((book, index) => {
                     return (
                         <div
                             key={i} onClick={() => singleBookPage(book.id)} className='basis-1/8 xl:basis-1/4 cursor-pointer bg-amber-100 hover:scale-105 m-3 my-6 lg:min-w-[20vw] xl:min-w-[15vw] md:min-w-[25vw] duration-200 *:duration-150 min-w-[50vw] max-w-fit w-auto aspect-2/3 relative border-s-8 border-amber-200'
